167f74520e78fdebed29f02370e032573f419edb
Juan <juan@MacBook-Pro.local>
2022-08-25 12:48:36 +0200

    feat: blocks explaining

--- src/components/body/hero.js
+++ src/components/body/hero.js
@@ -2,6 +2,8 @@ import {
     Box,
     Text,
     Heading,
+    Divider,
+    SimpleGrid,
     Button
   } from '@chakra-ui/react';
 import { Stack } from '@chakra-ui/react';
@@ -26,9 +28,24 @@ function Hero() {
             zk-tech and financial marketplaces 
             </Text>
           </Heading>
-          <Text color = {'blue.900'}>
-            Currently building the tooling and infrastructure for high-growth companies to access more efficient global debt markets. <br/>
-          </Text>
+      <SimpleGrid columns={5} spacing={5} className='my-box'> 
+          <Box bg='clear' height='120px'> </Box>
+          <Box boxShadow='xl' _hover={{boxShadow: '2xl'}} bg='gray.200' rounded={10} height='250px'>
+              <Text paddingTop={1} paddingBottom={3} fontWeight='bold'>SIROX FINANCE</Text>
+              <Divider orientation='horizontal'/>
+              <Text as={'span'}>Building the infrastructure for next-generation debt facilities</Text>
+          </Box>
+          <Box boxShadow='xl' _hover={{boxShadow: '2xl'}} bg='gray.200' rounded={10} height='250px'>
+            <Text paddingTop={1} paddingBottom={3}fontWeight='bold'>YIELD FARMING</Text>
+            <Text>We build our own smart contracts and tooling to extract yield from markets </Text>
+          </Box>
+          <Box boxShadow='xl' _hover={{boxShadow: '2xl'}} bg='gray.200' rounded={10} height='250px'>
+          <Text paddingTop={1} paddingBottom={3} fontWeight='bold'>ANGEL INVESTING</Text>
+          <Text>We provide angel tickets in interesting projects where our experience might be valuable.
+      Currently interested in on-chain options and verifiable computation</Text>
+      </Box>
+          <Box bg='clear' height='120px'> </Box>
+      </SimpleGrid>
           <Stack 
           direction={'column'}
           spacing={3}
@@ -41,7 +58,6 @@ function Hero() {
                <Text color = {'gray.700'} fontWeight="extrabold">
                 hey@siroexmon.xyz
               </Text>
- 
                <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
                 About us 
             </Button> 
