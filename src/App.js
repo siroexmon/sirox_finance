75aae1b20276686551d10f015d44f51d6f73eee2
Gonzalo Bandeira <bandeira@siroexmon.xyz>
2023-07-03 16:13:57 +0200

    feat: using a router

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import React from 'react';
-import { ChakraProvider, theme, Box, Flex} from '@chakra-ui/react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { ChakraProvider, theme, Box, Flex } from '@chakra-ui/react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Body from './components/body';
 import Header from './components/header';
 import Footer from './components/footer';
+import PrivacyPage from './components/PrivacyPage'
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" marginBottom={'0px'}>
-        <Flex minH="100vh" flexDirection="column" justifyContent="space-between" bgColor="#f3f3f3" color="#003157">
-          <Header/>
-          <HashRouter>
+      <Router>
+        <Box textAlign="center" marginBottom={'0px'}>
+          <Flex minH="100vh" flexDirection="column" justifyContent="space-between" bgColor="#f3f3f3" color="#003157">
+            <Header />
             <Routes>
               <Route path="/" element={<Body />} />
-              <Route path="/privacy" element={redirectToPDF('https://fence-assets.s3.eu-central-1.amazonaws.com/t%26c_privacy/fence_privacy_policy_20230703.pdf')} />
-              <Route path="/terms" element={redirectToPDF('https://fence-assets.s3.eu-central-1.amazonaws.com/t%26c_privacy/fence_t%26c_20230703.pdf')} />
-              <Route path="/cookie" element={redirectToPDF('https://fence-assets.s3.eu-central-1.amazonaws.com/t%26c_privacy/fence_cookie_20230703.pdf')} />
+              <Route path="/privacy" element={<PrivacyPage />} />
             </Routes>
-          </HashRouter>
-          <Footer/>
-        </Flex>
-      </Box>
+            <Footer />
+          </Flex>
+        </Box>
+      </Router>
     </ChakraProvider>
   );
-}
-
-function redirectToPDF(url) {
-  window.location.href = url;
-  return null;
 }
 
 export default App;
---
+++ src/components/PrivacyPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box, Heading, Text } from '@chakra-ui/react';
+
+function PrivacyPage() {
+  return (
+    <Box>
+      <Heading as="h1" size="xl" mb="4">
+        Privacy Policy
+      </Heading>
+      <Text>
+        This is the privacy policy content.
+      </Text>
+      {/* Add more content as needed */}
+    </Box>
+  );
+}
+
+export default PrivacyPage;
