d37ed0e0e7f28f2d20a48deea29295b78dc4eca2
Gonzalo Bandeira <bandeira@siroexmon.xyz>
2023-07-03 16:44:27 +0200

    version

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { ChakraProvider, theme, Box, Flex } from '@chakra-ui/react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Body from './components/body';
 import Header from './components/header';
 import Footer from './components/footer';
-import PrivacyPage from './components/PrivacyPage'
+import PrivacyPage from './components/PrivacyPage';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Router basename="/">
+      <Router>
         <Box textAlign="center" marginBottom={'0px'}>
           <Flex minH="100vh" flexDirection="column" justifyContent="space-between" bgColor="#f3f3f3" color="#003157">
             <Header />
-            <Routes>
-              <Route path="/" element={<Body />} />
-              <Route path="/privacy" element={<PrivacyPage />} />
-            </Routes>
+            <Switch>
+              <Route exact path="/" component={Body} />
+              <Route path="/privacy" component={PrivacyPage} />
+            </Switch>
             <Footer />
           </Flex>
         </Box>
