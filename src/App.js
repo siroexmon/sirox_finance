60b96df7393465cf7667a003f068d4efb9200bf6
Gonzalo Bandeira <bandeira@siroexmon.xyz>
2023-07-03 14:59:17 +0200

    feat: testing rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as  Router, Routes, Route } from 'react-router-dom';
 import { ChakraProvider, theme, Box, Flex} from '@chakra-ui/react'
+//import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Body from './components/body'
 import Header from './components/header'
 import Footer from './components/footer';
@@ -13,13 +14,12 @@ function App() {
           <Header/>
     <Router>
       <Routes>
-        {/* other routes */}
+        <Route path="/" element={<Body />} />
         <Route path="/privacy" element={<PDFDownload href="https://fence-assets.s3.eu-central-1.amazonaws.com/t%26c_privacy/fence_privacy_policy_20230703.pdf" />} />
         <Route path="/terms" element={<PDFDownload href="https://fence-assets.s3.eu-central-1.amazonaws.com/t%26c_privacy/fence_t%26c_20230703.pdf" />} />
         <Route path="/cookie" element={<PDFDownload href="https://fence-assets.s3.eu-central-1.amazonaws.com/t%26c_privacy/fence_cookie_20230703.pdf" />} />
       </Routes>
     </Router>
-          <Body/>
           <Footer/>
         </Flex>
       </Box>
