b72ddd779e5cd0cf22d675989286d558b7eaa9a3
Gonzalo Bandeira <bandeira@siroexmon.xyz>
2023-07-03 15:28:45 +0200

    feat: alternative

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import React from 'react';
-import { ChakraProvider, theme, Box, Flex} from '@chakra-ui/react'
-//import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Body from './components/body'
-import Header from './components/header'
+import { ChakraProvider, theme, Box, Flex} from '@chakra-ui/react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Body from './components/body';
+import Header from './components/header';
 import Footer from './components/footer';
-import PDFDownload from './components/PDFDownload';
+
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" marginBottom={'0px'}>
         <Flex minH="100vh" flexDirection="column" justifyContent="space-between" bgColor="#f3f3f3" color="#003157">
           <Header/>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Body />} />
-        <Route path="/privacy" element={<PDFDownload href="https://fence-assets.s3.eu-central-1.amazonaws.com/t%26c_privacy/fence_privacy_policy_20230703.pdf" />} />
-        <Route path="/terms" element={<PDFDownload href="https://fence-assets.s3.eu-central-1.amazonaws.com/t%26c_privacy/fence_t%26c_20230703.pdf" />} />
-        <Route path="/cookie" element={<PDFDownload href="https://fence-assets.s3.eu-central-1.amazonaws.com/t%26c_privacy/fence_cookie_20230703.pdf" />} />
-      </Routes>
-    </Router>
+          <HashRouter>
+            <Routes>
+              <Route path="/" element={<Body />} />
+              <Route path="/privacy" element={redirectToPDF('https://fence-assets.s3.eu-central-1.amazonaws.com/t%26c_privacy/fence_privacy_policy_20230703.pdf')} />
+              <Route path="/terms" element={redirectToPDF('https://fence-assets.s3.eu-central-1.amazonaws.com/t%26c_privacy/fence_t%26c_20230703.pdf')} />
+              <Route path="/cookie" element={redirectToPDF('https://fence-assets.s3.eu-central-1.amazonaws.com/t%26c_privacy/fence_cookie_20230703.pdf')} />
+            </Routes>
+          </HashRouter>
           <Footer/>
         </Flex>
       </Box>
     </ChakraProvider>
   );
+}
+
+function redirectToPDF(url) {
+  window.location.href = url;
+  return null;
 }
 
 export default App;
